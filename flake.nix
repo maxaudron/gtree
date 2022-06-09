656fc8e1091020747bea2f6114cb91b756e8361e
Max Audron <audron@cocaine.farm>
2022-06-09 17:06:15 +0200

    prepare release 1.0.0

--- flake.nix
+++ flake.nix
@@ -1,4 +1,13 @@
 {
+  nixConfig = {
+    substituters =
+      [ "https://cache.nixos.org/" "https://nix-community.cachix.org" "https://nix.cache.vapor.systems" ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nix.cache.vapor.systems-1:OjV+eZuOK+im1n8tuwHdT+9hkQVoJORdX96FvWcMABk="
+    ];
+  };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     rust-overlay = {
